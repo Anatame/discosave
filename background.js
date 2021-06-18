556b0069c293c326987b2f3f8ac98cee05786607
Anatame <anata.me97@gmail.com>
2021-06-19 00:43:20 +0530

    Added interval function and fixed the infinite loop

--- background.js
+++ background.js
@@ -1,17 +1,12 @@
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
+  chrome.storage.sync.set({
+    color
+  });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
 
-chrome.storage.sync.onChanged.addListener(function (changes) {
-  let message = changes['message'];
-  if (message.newValue != null) {
-    // YOUR CODE HERE
-    console.log(message.newValue)
-  }
-});
 
 
 
@@ -19,16 +14,17 @@ chrome.webNavigation.onCompleted.addListener(function (tab) {
   if (tab.frameId == 0) {
     console.log("activated")
     startListening()
-    // activateScript();
   }
 });
 
 
-
-
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    if (request.msg == "startFunc") startListening();
+    if (request.msg == "startFunc") {
+      startListening()
+    } else if (request.msg == "be") {
+      activateScript();
+    }
   }
 );
 
@@ -37,22 +33,17 @@ function startListening() {
   chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
 
-
-      if (details.url.includes(' https://discord.com/api/v9/science')) {
-        console.log('ready')
-        // activateScript();
-      }
       if (details.url.includes('https://discord.com/api/v9/channels/')) {
         console.log('called')
         activateScript();
       }
-    },
-    { urls: ["<all_urls>"] }
+    }, {
+      urls: ["<all_urls>"]
+    }
   );
 
 }
 
-//scrollerInner-2YIMLh
 
 
 async function activateScript() {
@@ -69,38 +60,11 @@ async function activateScript() {
     },
     function: injectMain,
   });
-
-
 }
 
 function injectMain() {
-  //  chrome.storage.sync.get("color", ({ color }) => {
-  //    document.body.style.backgroundColor = color;
-  //  });
 
-  // let scrollElement = document.querySelectorAll(".scrollerInner-2YIMLh");
-  // console.log(scrollElement.children)
-
-  const observer = new MutationObserver(function (mutations) {
-    mutations.forEach((mutation) => {
-      if (mutation.addedNodes.length) {
-        console.log(mutation.addedNodes)
-        add();
-      }
-    })
-  })
-
-  //const bears = document.querySelector('.content-yTz4x3')
-  const bears = document.querySelector('.content-yTz4x3')
-  //
-
-  observer.observe(bears, {
-    childList: true,
-  })
-
-  add()
-
-  function add() {
+  window.setInterval(() => {
     let mainElement = document.querySelectorAll(".cozyMessage-3V1Y8y");
     if (mainElement) {
       console.log("defined")
@@ -128,12 +92,16 @@ function injectMain() {
                   console.log("contained");
                   console.log(messageContainer.childNodes[1].innerHTML);
 
-                  chrome.storage.sync.set({ message: messageContainer.childNodes[1].innerHTML });
+                  chrome.storage.sync.set({
+                    message: messageContainer.childNodes[1].innerHTML
+                  });
                 } else {
                   console.log("alone");
                   console.log(messageContainer.childNodes[2].innerHTML);
 
-                  chrome.storage.sync.set({ message: messageContainer.childNodes[2].innerHTML });
+                  chrome.storage.sync.set({
+                    message: messageContainer.childNodes[2].innerHTML
+                  });
                 }
               });
             } catch (err) {
@@ -144,8 +112,11 @@ function injectMain() {
         });
       });
     }
+    console.log("Interval")
+  }, 2000)
+
+  
   }
 
 
 
-}
