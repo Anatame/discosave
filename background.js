0a785d96ddded7f0ba71e72a54bc52142ada8173
Anatame <anata.me97@gmail.com>
2021-06-19 22:11:09 +0530

    Fixed messages with reply content not getting printed and fixed buttons not getting added when reaction permissions are absent

--- background.js
+++ background.js
@@ -24,8 +24,7 @@ chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.msg == "startFunc") {
       // startListening()
-    }
-        else if(request.msg == "be"){
+    } else if (request.msg == "be") {
       activateScript()
     }
   }
@@ -67,7 +66,9 @@ async function activateScript1() {
 function injectMain1() {
 
   window.setInterval(() => {
-    chrome.runtime.sendMessage({ msg: "be" })
+    chrome.runtime.sendMessage({
+      msg: "be"
+    })
     console.log("Injected script 1")
   }, 2000)
 }
@@ -91,77 +92,78 @@ async function activateScript() {
 function injectMain() {
 
   // function activate(){
-    let mainElement = document.querySelectorAll(".cozyMessage-3V1Y8y");
-    if (mainElement) {
-      console.log("defined")
+  let mainElement = document.querySelectorAll(".cozyMessage-3V1Y8y");
+  if (mainElement) {
+    console.log("defined")
 
-      mainElement.forEach((item) => {
-        let btnNum = 0;
-        item.addEventListener("mouseover", (event) => {
-          // console.log(item.childNodes[2].childNodes[0].childNodes[0])
-          if (btnNum == 0) {
-            try {
-              var buttonGroupDiv
-              if (item.childNodes.length == 3) {
-                buttonGroupDiv = item.childNodes[2].childNodes[0].childNodes[0];
-              } else if (item.childNodes.length == 4) {
-                buttonGroupDiv = item.childNodes[3].childNodes[0].childNodes[0];
-              }
-
-            if (buttonGroupDiv.childNodes.length == 3) {
-                
-                var button = document.createElement("BUTTON");
-                button.innerHTML = "Button";
-                buttonGroupDiv.style.backgroundColor = "red";
-                buttonGroupDiv.appendChild(button);
-                btnNum = 1;
-                button.addEventListener("click", (event) => {
-                  console.log("clicked");
-  
-                  var messageContainer = item.childNodes[0];
-  
-                  console.log(messageContainer.childNodes.length != 3);
-  
-                  if (messageContainer.childNodes.length == 2) {
-                    console.log("contained");
-                    console.log(messageContainer.childNodes[1].innerHTML);
-  
-                    chrome.storage.sync.set({
-                      message: messageContainer.childNodes[1].innerHTML
-                    });
-                  } else if(messageContainer.childNodes.length == 3) {
-                    console.log("alone");
-                    console.log(messageContainer.childNodes[2].innerHTML);
-  
-                    chrome.storage.sync.set({
-                      message: messageContainer.childNodes[2].innerHTML
-                    });
-                  } else if(messageContainer.childNodes.length == 4) {
-                    console.log("alone");
-                    console.log(messageContainer.childNodes[3].innerHTML);
-  
-                    chrome.storage.sync.set({
-                      message: messageContainer.childNodes[3].innerHTML
-                    });
-                  }
-  
-                  
-                });
-              }
-        
-          
-            } catch (err) {
-              // if any error, Code throws the error
-              // console.log("not found")
+    mainElement.forEach((item) => {
+      let btnNum = 0;
+      item.addEventListener("mouseover", (event) => {
+        // console.log(item.childNodes[2].childNodes[0].childNodes[0])
+        if (btnNum == 0) {
+          try {
+            var buttonGroupDiv
+            var messageContainer
+            if (item.childNodes.length == 3) {
+              buttonGroupDiv = item.childNodes[2].childNodes[0].childNodes[0];
+              messageContainer = item.childNodes[0];
+            } else if (item.childNodes.length == 4) {
+              buttonGroupDiv = item.childNodes[3].childNodes[0].childNodes[0];
+              messageContainer = item.childNodes[1];
             }
+
+            if (buttonGroupDiv.childNodes.length <= 3 && !buttonGroupDiv.childNodes[buttonGroupDiv.childNodes.length - 1].classList.contains("btn")) {
+
+              var button = document.createElement("BUTTON");
+              button.innerHTML = "Button";
+              button.classList.add('btn')
+              buttonGroupDiv.style.backgroundColor = "red";
+              buttonGroupDiv.appendChild(button);
+              btnNum = 1;
+              button.addEventListener("click", (event) => {
+                console.log("clicked");
+
+                
+
+                console.log(messageContainer.childNodes.length != 3);
+
+                if (messageContainer.childNodes.length == 2) {
+                  console.log("contained");
+                  console.log(messageContainer.childNodes[1].innerHTML);
+
+                  chrome.storage.sync.set({
+                    message: messageContainer.childNodes[1].innerHTML
+                  });
+                } else if (messageContainer.childNodes.length == 3) {
+                  console.log("alone");
+                  console.log(messageContainer.childNodes[2].innerHTML);
+
+                  chrome.storage.sync.set({
+                    message: messageContainer.childNodes[2].innerHTML
+                  });
+                } else if (messageContainer.childNodes.length == 4) {
+                  console.log("containsReply");
+                  console.log(messageContainer.childNodes[3].innerHTML + " ahh");
+
+                  chrome.storage.sync.set({
+                    message: messageContainer.childNodes[3].innerHTML
+                  });
+                }
+
+
+              });
+            }
+
+
+          } catch (err) {
+            // if any error, Code throws the error
+            // console.log("not found")
           }
-        });
+        }
       });
-    }
-    console.log("Interval")  
-
-  
+    });
   }
+  console.log("Interval")
 
 
-
+}
