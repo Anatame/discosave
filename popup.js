5148685be8a4e08835614e5f86fbf0919e53dcbd
Anatame <anata.me97@gmail.com>
2021-06-22 15:10:13 +0530

    Extension sends an XHR request to local host server when authorize button from the popup is clicked

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
+let button = document.getElementById("authorize")
 
 
 let msArray = ["aa", "bb", "cc", "dd", "ee", "fff"]
@@ -7,48 +8,23 @@ let msArray = ["aa", "bb", "cc", "dd", "ee", "fff"]
 let div = document.createElement("div");
 let list = document.querySelector(".list")
 
+button.addEventListener("click", (e) => {
+  const Http = new XMLHttpRequest();
+  const url = 'http://127.0.0.1:5000/items';
+  Http.open("POST", url);
+  Http.send(`{"name": "bruh"}`);
 
-chrome.storage.sync.get(["color", "message", "messageArray"], ({
-  color,
-  message,
-  messageArray
-}) => {
-  if (messageArray) {
-    messageArray.push(message)
-
-
-    messageArray.forEach((item) => {
-      let li = document.createElement("div")
-      li.classList.add('listItems')
-      li.innerText = item
-      list.appendChild(li)
-      console.log(li)
-    })
-
-
-  } else {
-    chrome.storage.sync.set({
-      "messageArray": [message]
-    })
-    let li = document.createElement("li")
-    li.innerText = message
-    list.appendChild(li)
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText)
   }
 })
 
 
-//fetchMessages
-// chrome.storage.sync.get(["color", "message"], ({ color, message }) => {
-//   changeColor.style.backgroundColor = color;
-//   let div = document.createElement("div");
-//   div.innerHTML = message;
-//   document.body.appendChild(div);
-// });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-//   chrome.runtime.sendMessage({ msg: "startFunc" });
-// });
+// chrome.storage.sync.get(["color", "message", "messageArray"], ({
+//   color,
+//   message,
+//   messageArray
+// }) => {
 
-// The body of this function will be executed as a content script inside the
-// current page
+// })
