71174915fda96995b64049d9640d9a91c64434dd
Anatame <anata.me97@gmail.com>
2021-06-25 20:54:17 +0530

    Added auth button style functionality and channel popup display

--- Legacy/popup-sign-out-script.js
+++ Legacy/popup-sign-out-script.js
@@ -1,6 +1,15 @@
 let button = document.getElementById("logout")
 let avatar = document.getElementById("avatar")
-let avatar = document.getElementById("avatar")
+let username = document.getElementById("userName")
+let discriminator = document.createElement('span')
+let userID = document.getElementById("userID")
+
+let authorizeBotContainer = document.querySelector('.authorize-bot-container')
+let channelContainer = document.querySelector('.channelContainer')
+let authBotBtn = document.getElementById('authorizeBotBtn')
+
+let channelContainerBtn = document.querySelector(".channelContainerBtn")
+let list = document.querySelector(".list")
 
 button.addEventListener("click", (e) => {
    chrome.runtime.sendMessage({ msg: 'login' }, (response) => {
@@ -12,6 +21,20 @@ chrome.storage.sync.get(
    ["color", "message", "svg", "svgHover", "svgClick", "profile"],
    ({ color, message, svg, svgHover, svgClick, profile }) => {
       avatar.src = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png`
-      console.log(data)
+      username.innerText = profile.username
+      discriminator.innerText = `#${profile.discriminator}`;
+      discriminator.classList.add('tag')
+      username.appendChild(discriminator)
+      userID.innerText  = profile.id
    }
  );
+
+authBotBtn.addEventListener("click", (e) => {
+   console.log("click")
+   authorizeBotContainer.style.display = "none"
+   channelContainer.style.display = "block"
+})
+
+channelContainerBtn.addEventListener("click", (e) => {
+   list.style.display = "block"
+})
